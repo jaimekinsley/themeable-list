a96683f5078c49785cfee119d061e81e31fca864
Jaime Sanders <jaimelyn517@gmail.com>
2020-08-03 19:17:57 -0700

    fixes linting issues

--- src/components/characters/Characters.jsx
+++ src/components/characters/Characters.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import { useCharacters, useTheme } from '../../hooks/appContext';
-import styles from './Characters.css';
+import { useCharacters } from '../../hooks/appContext';
 
 const Characters = () => {
-  const theme = useTheme();
   const characters = useCharacters();
 
-  if(!characters) return <h2>Loading...</h2>
+  if(!characters) return <h2>Loading...</h2>;
   const characterElements = characters.map(character => (
     <li key={character._id}>
       <h3>{character.name}</h3>
       <h5>{character.affiliation}</h5>
       <img src={character.photoUrl}/>
     </li>
-  ))
+  ));
 
-  return(
+  return (
     <>
-    <ul>
-      {characterElements}
-    </ul>
+      <ul>
+        {characterElements}
+      </ul>
     </>
-  )
-}
+  );
+};
 
 export default Characters;
